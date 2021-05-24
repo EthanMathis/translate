66e79a00b3af17bc9e4fc70d162bc51275e2de9a
EthanMathis <ethan.mathis@outlook.com>
2021-05-24 13:24:55 -0500

    finished rolling dice

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,18 @@
-// Put your code here
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+const dieRoll = () => Math.floor(Math.random() * 6) + 1
+
+const rollTen = () => {
+    for (let i = 0; i < 10; i++) {
+        const roll1 = dieRoll();
+        const roll2 = dieRoll();
+        if (roll1 === roll2) {
+            console.log(`${roll1} + ${roll2} == ${roll1 + roll2} DOUBLES!`)
+        } else {
+            console.log(`${roll1} + ${roll2} == ${roll1 + roll2}`)
+        }
+    }
+}
+
+rollTen()
