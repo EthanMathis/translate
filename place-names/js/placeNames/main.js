9dd3fc1d53dd5664380bf3afa3c54fde44dc0920
EthanMathis <ethan.mathis@outlook.com>
2021-05-24 11:26:31 -0500

    finished Place Names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,22 @@
-// Put your code here
+const names = [
+    "Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China"
+]
+
+console.log("All Place Names")
+names.map(name => console.log(name))
+
+console.log("")
+
+console.log("'The' Place Names")
+names.map(name => {
+    if (name.startsWith("The")) {
+        console.log(name)
+    }
+})
